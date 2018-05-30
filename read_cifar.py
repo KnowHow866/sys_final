69b777e800f6fea0990028097c6df03ee91c408b
KnowHow866 <accidental001@gmail.com>
2018-05-31 01:56:25 +0800

    fix

--- read_cifar.py
+++ read_cifar.py
@@ -27,11 +27,14 @@ def unpickle(path):
 def reshape_cifar(picture_arr):
     tmp = []
     for img_data in picture_arr:
+        print(img_data.shape)
         img_R = img_data[0:1024].reshape((32, 32))
         img_G = img_data[1024:2048].reshape((32, 32))
         img_B = img_data[2048:3072].reshape((32, 32))
         img = np.dstack((img_R, img_G, img_B))
         tmp.append(img)
+        print(img.shape)
+    print(len(tmp))
     return tmp
 
 def label_map(lable_arr):
