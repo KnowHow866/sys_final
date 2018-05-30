f9c2907c566a62ff7a055a2ca984b2b583b1bec0
KnowHow866 <accidental001@gmail.com>
2018-05-31 01:29:39 +0800

    feat(inint training feature)

---
+++ model/alex_model.py
@@ -0,0 +1,43 @@
+
+import tensorflow as tf
+from tensorflow.python.keras.models import Sequential
+from tensorflow.python.keras.layers import Dense
+from tensorflow.python.keras.layers import Conv2D
+from tensorflow.python.keras.layers import MaxPool2D
+from tensorflow.python.keras.layers import Flatten
+
+import h5py
+import time
+import os 
+
+# Alex structure
+def alexnet():
+    input_shape = (32, 32, 3)
+    model = Sequential([
+        Conv2D(64, (3, 3), input_shape=input_shape, padding='same',
+            activation='relu'),
+        Conv2D(64, (3, 3), activation='relu', padding='same'),
+        MaxPool2D(pool_size=(2, 2), strides=(2, 2)),
+        Conv2D(128, (3, 3), activation='relu', padding='same'),
+        Conv2D(128, (3, 3), activation='relu', padding='same',),
+        MaxPool2D(pool_size=(2, 2), strides=(2, 2)),
+        Conv2D(256, (3, 3), activation='relu', padding='same',),
+        Conv2D(256, (3, 3), activation='relu', padding='same',),
+        Conv2D(256, (3, 3), activation='relu', padding='same',),
+        MaxPool2D(pool_size=(2, 2), strides=(2, 2)),
+        Conv2D(512, (3, 3), activation='relu', padding='same',),
+        Conv2D(512, (3, 3), activation='relu', padding='same',),
+        Conv2D(512, (3, 3), activation='relu', padding='same',),
+        MaxPool2D(pool_size=(2, 2), strides=(2, 2)),
+        Conv2D(512, (3, 3), activation='relu', padding='same',),
+        Conv2D(512, (3, 3), activation='relu', padding='same',),
+        Conv2D(512, (3, 3), activation='relu', padding='same',),
+        MaxPool2D(pool_size=(2, 2), strides=(2, 2)),
+
+        Flatten(),
+        Dense(4096, activation='relu'),
+        Dense(4096, activation='relu'),
+        Dense(1000, activation='softmax')
+    ])
+
+    return model
--- read_cifar.py
+++ read_cifar.py
@@ -1,7 +1,11 @@
+import tensorflow as tf
 import argparse
 import pickle
-import os
+import os 
+dir_path = os.path.dirname(os.path.realpath(__file__))
 from datetime import datetime
+import h5py
+import time
 import numpy as np
 import random
 
@@ -10,12 +14,43 @@ import matplotlib
 matplotlib.use('Agg')
 from matplotlib import pyplot as plt
 
+# model
+from model.alex_model import alexnet
+
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
 def unpickle(path):
     with open(path, 'rb') as file:
         tmp = pickle.load(file, encoding='bytes')
     return tmp
+
+def reshape_cifar(picture_arr):
+    tmp = []
+    for img_data in picture_arr:
+        img_R = img_dat[0:1024].reshape((32, 32))
+        img_G = img_dat[1024:2048].reshape((32, 32))
+        img_B = img_dat[2048:3072].reshape((32, 32))
+        img = np.dstack((img_R, img_G, img_B))
+        tmp.append(img)
+    return tmp
+
+def label_map(lable_arr):
+    lable_dict = {
+        1: 'airplane',
+        2: 'automobile',
+        3: 'bird',
+        4: 'cat',
+        5: 'deer',
+        6: 'dog',
+        7: 'frog',
+        8: 'horse',
+        9: 'ship',
+        10: 'truck'
+    }
+    for idx in range(len(lable_arr)):
+        lable_arr[idx] = lable_dict.get(lable_arr[idx], lable_arr[idx])
+    return lable_arr
+    
 
 def save_img(img, label='Default'):
     print('Ready to save image: %s' % label)
@@ -34,13 +69,20 @@ def main():
 
     data = unpickle(args.data)
     # save test
-    for idx in range(30):
-        img_R = data[b'data'][idx][0:1024].reshape((32, 32))
-        img_G = data[b'data'][idx][1024:2048].reshape((32, 32))
-        img_B = data[b'data'][idx][2048:3072].reshape((32, 32))
-        img = np.dstack((img_R, img_G, img_B))
-        save_img(img, str(data[b'labels'][idx]))
-    print('Save success')
+    # for idx in range(30):
+    #     img_R = data[b'data'][idx][0:1024].reshape((32, 32))
+    #     img_G = data[b'data'][idx][1024:2048].reshape((32, 32))
+    #     img_B = data[b'data'][idx][2048:3072].reshape((32, 32))
+    #     img = np.dstack((img_R, img_G, img_B))
+    #     save_img(img, str(data[b'labels'][idx]))
+    # print('Save success')
+
+    model = alexnet()
+    model.summary()
+
+    with tf.device_scope('/cpu:0'):
+        model.fit(reshape_cifar(data[b'data']), label_map(data[b'labels']), epochs=10, steps_per_epoch=32)
+        model.save('%s/model_saved/%s_model.h5' % (dir_path, datetime.now().strftime('%Y-%m-%d')))
 
     
 if __name__ == "__main__":
