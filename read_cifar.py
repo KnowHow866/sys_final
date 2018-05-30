2ea5441263bb00300ef714a23ecb490e917682d3
KnowHow866 <accidental001@gmail.com>
2018-05-31 02:36:16 +0800

    fix(gpu training)

--- read_cifar.py
+++ read_cifar.py
@@ -90,7 +90,7 @@ def main():
     with tf.device('/gpu:0'):
         print('start training')
         model.fit(reshape_cifar(data[b'data']), data[b'labels'], epochs=10, batch_size=32, verbose=1)
-        model.save('%s/model_saved/%s_model.h5' % (dir_path, datetime.now().strftime('%Y-%m-%d')))
+    model.save('%s/model_saved/%s_model.h5' % (dir_path, datetime.now().strftime('%Y-%m-%d')))
 
     
 if __name__ == "__main__":
