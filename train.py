6d2d70f31aa0bba05865bf3de08ab723f5aaa613
KnowHow866 <accidental001@gmail.com>
2018-06-24 21:06:00 +0800

    feat(normoilzation)

--- train.py
+++ train.py
@@ -39,6 +39,8 @@ def main():
 
     # dataset paths
     (x_train, y_train), (x_test, y_test) = cifar10.load_data()
+    x_train = x_train.astype('float32') / 255.0
+    x_test= x_test.astype('float32') / 255.0
     y_train = to_categorical(y_train)
     y_test = to_categorical(y_test)
 
