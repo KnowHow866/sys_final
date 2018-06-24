6dce7ccff7c9ae3ebc4a44f2f87cb2d5fb773434
KnowHow866 <accidental001@gmail.com>
2018-06-24 21:29:50 +0800

    fix(decrease dropout)

--- model/alexnet.py
+++ model/alexnet.py
@@ -40,7 +40,6 @@ model = Sequential([
     Flatten(),
     Dropout(0.2, noise_shape=None, seed=None),
     Dense(1024, activation='relu'),
-    Dropout(0.2, noise_shape=None, seed=None),
     Dense(10, activation='softmax')
 ])
 model.compile(loss='categorical_crossentropy',
