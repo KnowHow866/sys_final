d4134e251f8247c2dc5d21e6cd637425bc05c393
KnowHow866 <accidental001@gmail.com>
2018-06-30 10:55:56 +0800

    fix(fool student 2)

--- model/gamma.py
+++ model/gamma.py
@@ -16,16 +16,16 @@ import os
 def get_model():
     input_shape = (32, 32, 3)
     model = Sequential([
-        Conv2D(32, (3, 3), input_shape=input_shape, padding='same',
+        Conv2D(16, (3, 3), input_shape=input_shape, padding='same',
             activation='relu'),
         Dropout(0.2, noise_shape=None, seed=None),
         MaxPool2D(pool_size=(2, 2), strides=(2, 2)),
 
-        Conv2D(64, (3, 3), activation='relu', padding='same'),
+        Conv2D(32, (3, 3), activation='relu', padding='same'),
         Dropout(0.2, noise_shape=None, seed=None),
         MaxPool2D(pool_size=(2, 2), strides=(2, 2)),
 
-        Conv2D(128, (3, 3), activation='relu', padding='same'),
+        Conv2D(64, (3, 3), activation='relu', padding='same'),
         Dropout(0.2, noise_shape=None, seed=None),
         MaxPool2D(pool_size=(2, 2), strides=(2, 2)),
 
